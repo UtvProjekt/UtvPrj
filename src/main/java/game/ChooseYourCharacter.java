cec84965eae59b70a0c8b9f90efb7f6a83db409a
Oscar <oscjon2001@skola.goteborg.se>
2021-02-01 10:36:02 +0100

    added health for the characters and a new method were you set the username

--- src/main/java/game/ChooseYourCharacter.java
+++ src/main/java/game/ChooseYourCharacter.java
@@ -7,40 +7,31 @@ public class ChooseYourCharacter {
 	private String userName;
 	private String typeOfSide;
 	private TheDiffrentCharacters typeOfCharacter;
+	private int healthOfCharacter;
 
+	public void CreateYourCharacter(Scanner input) {
 
-	public void CreateYourCharacter() {
-
-		try (Scanner input = new Scanner(System.in)) {
-
-			System.out.println("Create Username: ");
-			userName = input.next();
-
+		try (input) {
 			System.out.println("Pick a side [GOOD/BAD]: ");
 			typeOfSide = input.next();
 
-			
-			if(!typeOfSide.toLowerCase().equals("good") && !typeOfSide.toLowerCase().equals("bad")) {
+			if (!typeOfSide.toLowerCase().equals("good") && !typeOfSide.toLowerCase().equals("bad")) {
 				System.out.println("That is not valid side!");
-				
-				while(!typeOfSide.toLowerCase().equals("good") && !typeOfSide.toLowerCase().equals("bad")){
+
+				while (!typeOfSide.toLowerCase().equals("good") && !typeOfSide.toLowerCase().equals("bad")) {
 					System.out.print("Pick a side [GOOD/BAD]: ");
 					typeOfSide = input.next();
+				}
 
-				} 
-				
 			}
-			
-			
 
 			if (typeOfSide.toLowerCase().equals("good")) {
 				String answer;
 				System.out.println("Pick a character - [Angel/Faerie/ShieldBearer]: ");
 				answer = input.next();
-				
-				if(!answer.toLowerCase().equals("angel") && !answer.toLowerCase().equals("faerie")
+
+				if (!answer.toLowerCase().equals("angel") && !answer.toLowerCase().equals("faerie")
 						&& !answer.toLowerCase().equals("sheildbearer")) {
-					
 					do {
 						System.out.println("This character doesn't exist");
 						System.out.println("Pick a character - [ANGEL/FAERIE/SHEILDBEARER]: ");
@@ -48,27 +39,27 @@ public class ChooseYourCharacter {
 						answer = input.next();
 					} while (!answer.toLowerCase().equals("angel") && !answer.toLowerCase().equals("faerie")
 							&& !answer.toLowerCase().equals("sheildbearer"));
-					
-					
 				}
 				if (answer.toLowerCase().equals("angel")) {
 					typeOfCharacter = TheDiffrentCharacters.ANGEL;
-				}
-				else if (answer.toLowerCase().equals("faerie")) {
+					healthOfCharacter = 160;
+				} else if (answer.toLowerCase().equals("faerie")) {
 					typeOfCharacter = TheDiffrentCharacters.FAERIE;
-				}
-				else if (answer.toLowerCase().equals("sheildbearer")) {
+					healthOfCharacter = 200;
+				} else if (answer.toLowerCase().equals("sheildbearer")) {
 					typeOfCharacter = TheDiffrentCharacters.SHEILDBEARER;
-				} 
-				
-			}	
-			    if (typeOfSide.toLowerCase().equals("bad")) {
-			    String answer;
+					healthOfCharacter = 250;
+				}
+
+			}
+			if (typeOfSide.toLowerCase().equals("bad")) {
+				String answer;
 				System.out.println("Pick a character - [NightmareShadow/SoulReaper/Demon]: ");
 				answer = input.next();
-				
-				if(!answer.toLowerCase().equals("nightmareshadow") && !answer.toLowerCase().equals("soulreaper") && !answer.toLowerCase().equals("demon")) {
-					
+
+				if (!answer.toLowerCase().equals("nightmareshadow") && !answer.toLowerCase().equals("soulreaper")
+						&& !answer.toLowerCase().equals("demon")) {
+
 					do {
 						System.out.println("This character doesn't exist");
 						System.out.println("Pick a character - [NightmareShadow/SoulReaper/Demon]: ");
@@ -76,28 +67,29 @@ public class ChooseYourCharacter {
 						answer = input.next();
 					} while (!answer.toLowerCase().equals("nightmareshadow")
 							&& !answer.toLowerCase().equals("soulreaper") && !answer.toLowerCase().equals("demon"));
-					
+
 				}
 				if (answer.toLowerCase().equals("nightmareshadow")) {
 					typeOfCharacter = TheDiffrentCharacters.NIGHTMARE_SHADOW;
-				}
-				else if (answer.toLowerCase().equals("soulreaper")) {
+					healthOfCharacter = 160;
+				} else if (answer.toLowerCase().equals("soulreaper")) {
 					typeOfCharacter = TheDiffrentCharacters.SOUL_REAPER;
-				}
-				else if (answer.toLowerCase().equals("demon")) {
+					healthOfCharacter = 200;
+				} else if (answer.toLowerCase().equals("demon")) {
 					typeOfCharacter = TheDiffrentCharacters.DEMON;
+					healthOfCharacter = 250;
 				}
-			} 
-			
-		
-			
-			System.out.println(" hej" + userName + typeOfSide + typeOfCharacter);
-			
-			
-		} catch (Exception e) {
+			}
+		} 
+		catch (Exception e) {
 
 		}
 
+	}
+
+	public void setUserName(Scanner input) {
+		System.out.println("Create Username: ");
+		userName = input.next();
 	}
 
 	public String getUserName() {
@@ -110,6 +102,16 @@ public class ChooseYourCharacter {
 
 	public TheDiffrentCharacters getTypeOfCharacter() {
 		return typeOfCharacter;
+	}
+
+	public int getHealthOfCharacter() {
+		return healthOfCharacter;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("UserName: %s \nSide: %s \nClass: %s \nHealth: %d", userName, typeOfSide, typeOfCharacter,
+				healthOfCharacter);
 	}
 
 }
