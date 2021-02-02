11cfd91c256e3bd6c714dfd999d52efbc0fb6a74
Anton Ewards <antewa1212@skola.goteborg.se>
2021-02-02 11:11:23 +0100

    Added javadoc to file

--- src/main/java/game/TheStory.java
+++ src/main/java/game/TheStory.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class TheStory {
 
-	public void Story() {
+	public void startStory() {
 		
 		Scanner input = new Scanner(System.in);
 		String name;
@@ -12,6 +12,9 @@ public class TheStory {
 		AbilityDamage damage = new AbilityDamage();
 		AbilityBook book = new AbilityBook();
 		int i = 0;
+		
+		/*Story begins and you get to pick your name, side and character. 
+		 *Where "input.nextLine() makes you press Enter to move forward*/
 		
 		System.out.println("Welcome To The Final Boss.\n\nYour story begins in a small town called Teratopia as a lone student.\n\nYou've just entered the local"
 				+ " tavern and someone calls on you. (Enter)");
@@ -21,17 +24,21 @@ public class TheStory {
 		name = input.next();
 		player.setUserName(name);
 		
-		System.out.println("\nMy name is " + player.getUserName() + " and im new in town. Can i have your finest ale please?(Enter)");
+		System.out.println("\nMy name is " + player.getUserName() + " and im new in town. Can i have your finest ale please?");
 		input.nextLine();
 		
 		System.out.println("\nOf course my friend! But i have to ask.. I haven't seen your kind here before?\nWhat are you?\n");
-		System.out.println("(All) to look at the characters abilities. (Enter) to skip");
+		System.out.println("(All) to look at the characters abilities. (Enter) to skip\n");
+		
+		/*One time request to see all abilities for every character.*/
 		String all;
 		all = input.next();
 		if(all.toLowerCase().equals("all")){
 			book.printGlobal();
 		}
-		player.CreateYourCharacter(input);
+		else {
+			player.CreateYourCharacter(input);			
+		}
 		
 		
 		input.nextLine();
@@ -46,7 +53,29 @@ public class TheStory {
 		System.out.println("You've just entered the grand masters cave and prepared for the final boss, one versus one you fight to the death.(Enter)");
 		input.nextLine();
 		
+		/* Boss fight begins */
 		
+		while(true)
+        {
+            damage.PlayerAttack(player, i);
+            i++;
+            System.out.println("Your health:" + damage.getPlayerHealth() + " Boss health: " + damage.getBossHealth());
+            if(damage.getPlayerHealth()<=0 && damage.getBossHealth()<=0)
+            {
+                System.out.println("Both of you fell, shattering the ground with a draw!");
+                break;
+            }
+            else if(damage.getPlayerHealth()<=0)
+            {
+                System.out.println("Defeat! The boss crushed you.");
+                break;
+            }
+            else if(damage.getBossHealth()<=0)
+            {
+                System.out.println("The boss died, you stand victorious!");
+                break;
+            }
+        }
 		
 	}
 }
